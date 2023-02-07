4f092943a1e334fc91e4c8da16ea53cf819e73dc
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-07 21:33:31 +0900

    commit

--- Alert-UIkit/ViewController.swift
+++ Alert-UIkit/ViewController.swift
@@ -1,19 +1,45 @@
-//
-//  ViewController.swift
-//  Alert-UIkit
-//
-//  Created by 渡邊魁優 on 2023/02/07.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultLabel.text = "ボタンを押してね"
     }
 
-
+    @IBAction func alert1(_ sender: Any) {
+        alert(buttonNumber: "ボタン１",
+              message: "ボタン1が押されました")
+    }
+    @IBAction func alert2(_ sender: Any) {
+        alert(buttonNumber: "ボタン2",
+              message: "ボタン2が押されました")
+    }
+    @IBAction func alert3(_ sender: Any) {
+        alert(buttonNumber: "ボタン3",
+              message: "ボタン3が押されました")
+    }
+    private func alert(buttonNumber: String ,message: String) {
+        let alertControll = UIAlertController(
+            title: buttonNumber,
+            message: message,
+            preferredStyle: .alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: { action in
+                print("updateを呼びます")
+                self.updateLabel(text: buttonNumber)
+            }
+        )
+        alertControll.addAction(ok)
+        present(alertControll, animated: true)
+    }
+    private func updateLabel(text: String) {
+        print("updateが呼ばれました")
+        resultLabel.text = text + "が押されました"
+    }
 }
-
